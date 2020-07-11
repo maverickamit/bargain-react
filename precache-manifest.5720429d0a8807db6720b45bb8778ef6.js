b5e9b86b3b727c652938a304054dd1199da4d60c
Amit <29959138+maverickamit@users.noreply.github.com>
2020-07-11 13:16:26 +0530

    Updates

---
+++ precache-manifest.5720429d0a8807db6720b45bb8778ef6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ae6d4891528253eb80e9464fb13b7bb",
+    "url": "/react-product-alert/index.html"
+  },
+  {
+    "revision": "78383c4d14099e60e9e6",
+    "url": "/react-product-alert/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "bee18f6b923ac51139cc",
+    "url": "/react-product-alert/static/css/main.a80628f6.chunk.css"
+  },
+  {
+    "revision": "78383c4d14099e60e9e6",
+    "url": "/react-product-alert/static/js/2.a626f7a9.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-product-alert/static/js/2.a626f7a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bee18f6b923ac51139cc",
+    "url": "/react-product-alert/static/js/main.d1f00a10.chunk.js"
+  },
+  {
+    "revision": "4f5ee0dfd95c5fa14c14",
+    "url": "/react-product-alert/static/js/runtime-main.590ab53d.js"
+  }
+]);
