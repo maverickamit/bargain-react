1ca3f4c0da2f3b735c0ad555205a66a01a55e7cb
Amit <29959138+maverickamit@users.noreply.github.com>
2020-07-11 13:13:21 +0530

    adding new product via api

--- src/components/AddForm/AddForm.jsx
+++ src/components/AddForm/AddForm.jsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './AddForm.css';
+const axios = require('axios').default;
 
 function AddForm(email) {
+	const urlInput = useRef(null);
+
+	const serverUrl = 'https://x25iuvslok.execute-api.ap-south-1.amazonaws.com/dev/api/bargains';
+
+	const handleUrlSubmit = (event) => {
+		event.preventDefault();
+		console.log(urlInput.current.value);
+		axios({
+			method: 'post',
+			url: serverUrl,
+			data: {
+				email: email.email,
+				productUrl: urlInput.current.value
+			}
+		})
+			.then(function(response) {
+				console.log(response);
+			})
+			.catch(function(error) {
+				console.log(error);
+			});
+	};
 	return (
-		<form action="#">
+		<form
+			onSubmit={(event) => {
+				handleUrlSubmit(event);
+			}}
+		>
 			<div class="row">
 				<h5 style={{ paddingRight: '20px' }}>Toogle Subscription Alert</h5>
 				<input type="checkbox" name="fancy-checkbox" id="fancy-checkbox" />
@@ -12,8 +39,8 @@ function AddForm(email) {
 			<h5 class="text-help">Add Product</h5>
 
 			<div class="row">
-				<input type="text" name="fancy-text" id="fancy-text" />
-				<label for="fancy-text">Url</label>
+				<input type="text" name="product-url" id="product-url" ref={urlInput} />
+				<label for="product-url">Url</label>
 				<button type="submit" tabindex="0">
 					Submit
 				</button>
